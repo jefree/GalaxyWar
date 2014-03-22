e8cb51b8528c858e45bd625d1afc4cc97acef22d
jefree <jgarzon920429@gmail.com>
2014-03-21 22:14:50 -0500

    Change ship acceleration method

--- GalaxyWar/src/dev/jet/android/galaxywar/world/actors/Ship.java
+++ GalaxyWar/src/dev/jet/android/galaxywar/world/actors/Ship.java
@@ -28,7 +28,7 @@ public class Ship extends Entity {
 		if (rDelta > 0) {
 			
 			rotate(MAX_ROTATION_SPEED * rOrientation * rDelta * delta);
-			rDelta -= 5*delta;
+			rDelta = 0;
 		}
 		
 		float speed[] = UtilityMath.getSides(getSpeed()*delta, getRotation());
