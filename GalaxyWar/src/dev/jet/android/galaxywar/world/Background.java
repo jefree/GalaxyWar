bc1ba80be1cbc5b2a0dfcf36f4082af6e16a82dc
jefree <jgarzon920429@gmail.com>
2014-04-12 18:55:33 -0500

    fix error on Background. add sound attribute to Background

--- GalaxyWar/src/dev/jet/android/galaxywar/world/Background.java
+++ GalaxyWar/src/dev/jet/android/galaxywar/world/Background.java
@@ -1,5 +1,7 @@
 package dev.jet.android.galaxywar.world;
 
+import com.badlogic.gdx.audio.Sound;
+
 import dev.jet.android.galaxywar.media.Picture;
 import dev.jet.android.galaxywar.media.TapestryPicture;
 import dev.jet.android.galaxywar.world.actors.Entity;
@@ -10,8 +12,8 @@ public class Background extends Entity {
 	private static float INIT_BACK_Y = 0;
 	
 	@Override
-	public void create(World _world, Picture _picture) {
-		super.create(_world, _picture);
+	public void create(World _world, Picture _picture, Sound _sound) {
+		super.create(_world, _picture, _sound);
 		
 		((TapestryPicture)picture).setInitPosition(INIT_BACK_X, INIT_BACK_Y);
 
